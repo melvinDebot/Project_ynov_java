b5b406a0d3f509aa432d945074bff00caf67f13a
Melvin Debot <melvin@exceenis.com>
2022-04-08 11:06:23 +0200

    :white_check_mark: add test api

--- back/src/Main.java
+++ back/src/Main.java
@@ -1,6 +1,22 @@
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
 
-    public static void main(String [] args){
-        System.out.println("Hello World !!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/todos/1"))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
